5696bca51c877a20aa78f8ce202d3cf7878461d7
Mequam <blue9ja@gmail.com>
2022-08-31 22:01:19 -0400

    added new recipies made the cmd.py print out valid recpies if run with no cmds added #!/bin/python and chmod +x to the cmd.py

--- cmd.py
+++ cmd.py
@@ -1,3 +1,4 @@
+#!/bin/python
 import decraft
 #this is the command interface for the decrafting code
 
@@ -34,14 +35,35 @@ def decraftArgs(craft_dict,args):
 
 if __name__ == '__main__':
 	import sys
-	
+
+	#add more recipies
 	nms_crafting_dict = {
+	"Metal Plating":[(50,"Ferrite Dust")],
+	"The Works":[(1,"Base Computer"),(1,"Mining Operation"),(1,"Galactic Trade Terminal"),(1,"Portal"),(1,"Batterie"),(1,"Solar Panel")],
 	"Mining Operation":[(2,"Solar Panel"),(2,"Batterie"),(1,"Extractor"),(2,"Supply Depot"),(2,"Supply Pipe")],
 	"Solar Panel":[(1,"Metal Plating"),(30,"Gold"),(50,"Chromatic Metal")],
 	"Batterie":[(60,"Magnatized Ferrite"),(100, "Condensed Carbon")],
 	"Extractor":[(1,"Metal Plating"),(100,"Chromatic Metal")],
 	"Supply Depot":[(10,"Metal Plating")],
-	"Supply Pipe":[(25,"Ferrite Dust"),(10,"Carbon")]
+	"Supply Pipe":[(25,"Ferrite Dust"),(10,"Carbon")],
+	"Galactic Trade Terminal":[(25,"Magnatized Ferrite"),(3,"Microprocessor")],
+	"Microprocessor":[(40,"Chromatic Metal"),(1,"Carbon Nanotube")],
+	"Carbon Nanotube":[(50,"Carbon")],
+	"Portal":[(1,"Metal Plating"),(2,"Carbon Nanotube"),(40,"Sodium")],
+	"Large Refiner":[(200,"Chromatic Metal"),(100,"Sodium Nitrate"),(5,"Microprocessor")],
+	"Base Computer":[(30,"Chromatic Metal")],
+	"Roamer":[(5,"Metal Plating"),(2,"Ion Battery"),(50,"Paraffinium")],
+	"Ion Battery":[(10,"Cobalt"),(5,"Ferrite Dust")],
+	"Nitrogen Salt":[(50,"Condensed Carbon"),(250,"Nitrogen")],
+	"Enriched Carbon":[(250,"Radon"),(50,"Condensed Carbon")],
+	"Thermic Condensate":[(250,"Sulferine"),(50,"Condensed Carbon")],
+	"Semi Conductor":[(1,"Thermic Condensate"),(1,"Nitrogen Salt")],
+	"Super Conductor":[(1,"Semi Conductor"),(1,"Enriched Carbon")]
 	}
-	decraft.print_dictionary_vector(decraftArgs(nms_crafting_dict,sys.argv[1:]))
+	if len(sys.argv) > 1:
+		decraft.print_dictionary_vector(decraftArgs(nms_crafting_dict,sys.argv[1:]))
+	else:
+		print('valid options\n')
+		for key in nms_crafting_dict:
+			print(f'{key}')
 
